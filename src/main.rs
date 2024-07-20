1c46ca729e29fdc8e2b2b56219e1c596c71c3713
imqwerty <sharans.roy@gmail.com>
2024-07-20 16:22:00 +0530

    dont ask how any of this is working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::read_to_string;
+
+#[derive(Debug)]
 enum Keyword {
     // random keywords
     Import,
@@ -13,8 +16,13 @@ enum Keyword {
     UnsignedInteger16,
     UnsignedInteger32,
     UnsignedInteger64,
+    Float32,
+    Float64,
     Character,
     Boolean,
+
+    True,
+    False,
 
     // conditional
     If,
@@ -26,16 +34,15 @@ enum Keyword {
     Continue,
 }
 
+#[derive(Debug)]
 enum Operator {
     Assign,
 
     Add,
     AddAssign,
-    Increment,
 
     Subtract,
-    SubAssing,
-    Decrement,
+    SubAssign,
 
     Multiply,
     MulAssign,
@@ -52,9 +59,6 @@ enum Operator {
     BitOr,
     BitOrAssign,
 
-    BitNot,
-    BitNotAssign,
-
     And,
     Or,
     Not,
@@ -67,6 +71,7 @@ enum Operator {
     GreaterThanEqual,
 }
 
+#[derive(Debug)]
 enum Bracket {
     RoundOpen,
     RoundClose,
@@ -76,91 +81,216 @@ enum Bracket {
     CurlyClose,
 }
 
+#[derive(Debug)]
 enum Literal {
     Integer(i32),
     BigInteger(i128),
-    Float(f32),
-    BigFloat(f64),
+    Float(f64),
     Character(char),
-    Boolean(bool),
     Array(Vec<Literal>),
+    Identifier(Box<str>),
 }
 
+impl TryFrom<&str> for Literal {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, ()> {
+        if let Ok(i) = value.parse::<i32>() {
+            Ok(Self::Integer(i))
+        } else if let Ok(i) = value.parse::<i128>() {
+            Ok(Self::BigInteger(i))
+        } else if let Ok(i) = value.parse::<f64>() {
+            Ok(Self::Float(i))
+        } else if value.starts_with('\'') && value.ends_with('\'') {
+            if value.len() == 3 {
+                Ok(Self::Character(value.chars().nth(1).unwrap()))
+            } else {
+                if !value.starts_with("'\\") {
+                    return Err(());
+                }
+
+                match value.chars().nth(2).unwrap() {
+                    'n' => Ok(Self::Character('\n')),
+                    'r' => Ok(Self::Character('\r')),
+                    't' => Ok(Self::Character('\t')),
+                    '\\' => Ok(Self::Character('\\')),
+                    '0' => Ok(Self::Character('\0')),
+                    '\'' => Ok(Self::Character('\'')),
+                    '"' => Ok(Self::Character('\"')),
+                    _ => Err(()),
+                }
+            }
+        } else if value.starts_with('[') && value.ends_with(']') {
+            todo!()
+        } else {
+            let mut valid_first_character: Vec<char> = ('A'..='Z').chain('a'..='z').collect();
+            valid_first_character.push('_');
+
+            if !valid_first_character.contains(&value.chars().next().ok_or(())?) {
+                return Err(());
+            }
+
+            valid_first_character.extend('0'..='9');
+
+            if !value.chars().all(|x| valid_first_character.contains(&x)) {
+                return Err(());
+            }
+
+            Ok(Self::Identifier(value.into()))
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Symbol {
+    End,
+    Seperator,
+}
+
+#[derive(Debug)]
 enum Token {
     KeywordToken(Keyword),
     OperatorToken(Operator),
     BracketToken(Bracket),
     LiteralToken(Literal),
-    End,
+    SymbolToken(Symbol),
 }
 
-impl From<&str> for Token {
-    fn from(value: &str) -> Self {
+impl TryFrom<&str> for Token {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, ()> {
         match value {
-            "import" => Self::KeywordToken(Keyword::Import),
-            "struct" => Self::KeywordToken(Keyword::Struct),
-            "return" => Self::KeywordToken(Keyword::Return),
-            "i8" => Self::KeywordToken(Keyword::SignedInteger8),
-            "i16" => Self::KeywordToken(Keyword::SignedInteger16),
-            "i32" => Self::KeywordToken(Keyword::SignedInteger32),
-            "i64" => Self::KeywordToken(Keyword::SignedInteger64),
-            "u8" => Self::KeywordToken(Keyword::UnsignedInteger8),
-            "u16" => Self::KeywordToken(Keyword::UnsignedInteger16),
-            "u32" => Self::KeywordToken(Keyword::UnsignedInteger32),
-            "u64" => Self::KeywordToken(Keyword::UnsignedInteger64),
-            "char" => Self::KeywordToken(Keyword::Character),
-            "bool" => Self::KeywordToken(Keyword::Boolean),
-            "if" => Self::KeywordToken(Keyword::If),
-            "else" => Self::KeywordToken(Keyword::Else),
-            "while" => Self::KeywordToken(Keyword::While),
-            "for" => Self::KeywordToken(Keyword::For),
-            "in" => Self::KeywordToken(Keyword::In),
-            "break" => Self::KeywordToken(Keyword::Break),
-            "continue" => Self::KeywordToken(Keyword::Continue),
-            "=" => todo!(),
-            "+" => todo!(),
-            "+=" => todo!(),
-            "++" => todo!(),
-            "-" => todo!(),
-            "-=" => todo!(),
-            "--" => todo!(),
-            "*" => todo!(),
-            "*=" => todo!(),
-            "/" => todo!(),
-            "/=" => todo!(),
-            "%" => todo!(),
-            "%=" => todo!(),
-            "&" => todo!(),
-            "&=" => todo!(),
-            "|" => todo!(),
-            "|=" => todo!(),
-            "~" => todo!(),
-            "&&" => todo!(),
-            "||" => todo!(),
-            "!" => todo!(),
-            "==" => todo!(),
-            "!=" => todo!(),
-            "<" => todo!(),
-            ">" => todo!(),
-            "<=" => todo!(),
-            ">=" => todo!(),
-            "(" => todo!(),
-            ")" => todo!(),
-            "[" => todo!(),
-            "]" => todo!(),
-            "{" => todo!(),
-            "}" => todo!(),
-            other => Self::LiteralToken(Literal::from(other)),
+            "import" => Ok(Self::KeywordToken(Keyword::Import)),
+            "struct" => Ok(Self::KeywordToken(Keyword::Struct)),
+            "return" => Ok(Self::KeywordToken(Keyword::Return)),
+            "i8" => Ok(Self::KeywordToken(Keyword::SignedInteger8)),
+            "i16" => Ok(Self::KeywordToken(Keyword::SignedInteger16)),
+            "i32" => Ok(Self::KeywordToken(Keyword::SignedInteger32)),
+            "i64" => Ok(Self::KeywordToken(Keyword::SignedInteger64)),
+            "u8" => Ok(Self::KeywordToken(Keyword::UnsignedInteger8)),
+            "u16" => Ok(Self::KeywordToken(Keyword::UnsignedInteger16)),
+            "u32" => Ok(Self::KeywordToken(Keyword::UnsignedInteger32)),
+            "u64" => Ok(Self::KeywordToken(Keyword::UnsignedInteger64)),
+            "f32" => Ok(Self::KeywordToken(Keyword::Float32)),
+            "f64" => Ok(Self::KeywordToken(Keyword::Float64)),
+            "char" => Ok(Self::KeywordToken(Keyword::Character)),
+            "bool" => Ok(Self::KeywordToken(Keyword::Boolean)),
+            "if" => Ok(Self::KeywordToken(Keyword::If)),
+            "else" => Ok(Self::KeywordToken(Keyword::Else)),
+            "while" => Ok(Self::KeywordToken(Keyword::While)),
+            "for" => Ok(Self::KeywordToken(Keyword::For)),
+            "in" => Ok(Self::KeywordToken(Keyword::In)),
+            "break" => Ok(Self::KeywordToken(Keyword::Break)),
+            "continue" => Ok(Self::KeywordToken(Keyword::Continue)),
+            "true" => Ok(Self::KeywordToken(Keyword::True)),
+            "false" => Ok(Self::KeywordToken(Keyword::False)),
+            "=" => Ok(Self::OperatorToken(Operator::Assign)),
+            "+" => Ok(Self::OperatorToken(Operator::Add)),
+            "+=" => Ok(Self::OperatorToken(Operator::AddAssign)),
+            "-" => Ok(Self::OperatorToken(Operator::Subtract)),
+            "-=" => Ok(Self::OperatorToken(Operator::SubAssign)),
+            "*" => Ok(Self::OperatorToken(Operator::Multiply)),
+            "*=" => Ok(Self::OperatorToken(Operator::MulAssign)),
+            "/" => Ok(Self::OperatorToken(Operator::Divide)),
+            "/=" => Ok(Self::OperatorToken(Operator::DivAssign)),
+            "%" => Ok(Self::OperatorToken(Operator::Modulus)),
+            "%=" => Ok(Self::OperatorToken(Operator::ModAssign)),
+            "&" => Ok(Self::OperatorToken(Operator::BitAnd)),
+            "&=" => Ok(Self::OperatorToken(Operator::BitAndAssign)),
+            "|" => Ok(Self::OperatorToken(Operator::BitOr)),
+            "|=" => Ok(Self::OperatorToken(Operator::BitOrAssign)),
+            "&&" => Ok(Self::OperatorToken(Operator::And)),
+            "||" => Ok(Self::OperatorToken(Operator::Or)),
+            "!" => Ok(Self::OperatorToken(Operator::Not)),
+            "==" => Ok(Self::OperatorToken(Operator::Equal)),
+            "!=" => Ok(Self::OperatorToken(Operator::NotEqual)),
+            "<" => Ok(Self::OperatorToken(Operator::LessThan)),
+            ">" => Ok(Self::OperatorToken(Operator::GreaterThan)),
+            "<=" => Ok(Self::OperatorToken(Operator::LessThanEqual)),
+            ">=" => Ok(Self::OperatorToken(Operator::GreaterThanEqual)),
+            "(" => Ok(Self::BracketToken(Bracket::RoundOpen)),
+            ")" => Ok(Self::BracketToken(Bracket::RoundClose)),
+            "[" => Ok(Self::BracketToken(Bracket::SquareOpen)),
+            "]" => Ok(Self::BracketToken(Bracket::SquareClose)),
+            "{" => Ok(Self::BracketToken(Bracket::CurlyOpen)),
+            "}" => Ok(Self::BracketToken(Bracket::CurlyClose)),
+            ";" => Ok(Self::SymbolToken(Symbol::End)),
+            "," => Ok(Self::SymbolToken(Symbol::Seperator)),
+            other => Ok(Self::LiteralToken(Literal::try_from(other)?)),
         }
     }
 }
 
 fn parse(file: &str) -> Vec<Token> {
+    let mut iterator = file.chars().peekable();
+
     let mut tokens = Vec::new();
+    let mut current_token = String::new();
+
+    let mut in_char = false;
+    let mut prev_chr = '\u{0}';
+
+    while let Some(i) = iterator.next() {
+        if i.is_whitespace() && !in_char {
+            if !current_token.is_empty() {
+                tokens.push(current_token.as_str().try_into().unwrap());
+                current_token.clear();
+            }
+
+            continue;
+        }
+
+        current_token.push(i);
+
+        if !in_char {
+            if Token::try_from(current_token.as_str()).is_err() {
+                current_token.pop();
+
+                tokens.push(current_token.as_str().try_into().unwrap());
+                current_token.clear();
+
+                if i != ' ' {
+                    current_token.push(i);
+                }
+            }
+        }
+
+        if i == '\'' {
+            if in_char && prev_chr != '\\' {
+                tokens.push(current_token.as_str().try_into().unwrap());
+
+                current_token.clear();
+                in_char = false;
+            } else {
+                in_char = true;
+            }
+        }
+
+        if current_token == "-" {
+            if let (Some(Token::LiteralToken(_)), Some(i)) = (tokens.last(), iterator.peek()) {
+                if !i.is_ascii_alphanumeric() {
+                    continue;
+                }
+
+                tokens.push(current_token.as_str().try_into().unwrap());
+                current_token.clear();
+            }
+        }
+
+        prev_chr = i;
+        println!("{:?}", current_token);
+    }
+
+    if !current_token.is_empty() {
+        tokens.push(current_token.as_str().try_into().unwrap());
+    }
 
     tokens
 }
 
 fn main() {
-    println!("return, world!");
+    let buffer = read_to_string("./main.alim").unwrap();
+    let tokens = parse(&buffer);
+    println!("{:#?}", tokens);
 }
